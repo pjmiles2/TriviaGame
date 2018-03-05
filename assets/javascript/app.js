88485baa69232a90050e781552f62b0937d08b75
Patrick Miles <patrick.miles@gmail.com>
2018-03-04 23:52:44 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,38 @@
 
 var counter = 0;
 var wins = 0;
-var losses =0;
+var losses = 0;
 var userChoice;
 var current;
 var answerArray;
-
+var windowTimeout = 0;
+var resetTimeout = 0;
 
 var game = {
 
 
-question: ["Just Do It", "Fly the Friendly Skies", "The Ultimate Driving Machine"],
-industry: ["shoe company's", "airline's", "car maker's"],
-correctAnswer: ["Nike", "United", "BMW"],
-wrongChoice1: ["Reebok", "Delta", "Lexus"],
-wrongChoice2: ["Adidas", "American", "Ford"],
-wrongChoice3: ["Converse", "Southwest", "Cadillac"]
+question: ["Just Do It", "Fly the Friendly Skies", "The Ultimate Driving Machine","Taste the Rainbow"],
+industry: ["shoe company's", "airline's", "car maker's","candy's"],
+correctAnswer: ["Nike", "United", "BMW","Skittles"],
+wrongChoice1: ["Reebok", "Delta", "Lexus", "M&M's"],
+wrongChoice2: ["Adidas", "American", "Ford", "Reese's Pieces"],
+wrongChoice3: ["Converse", "Southwest", "Cadillac", "Starburst"]
 
 }
 
+
 function newQuestion(){
+
+    $("button").trigger("reset");
+  
+
+clearTimeout(resetTimeout);
+clearTimeout(windowTimeout);
 
 var current = Math.floor(Math.random() * game.question.length);
 console.log(current);
     
+
 answerArray = [game.correctAnswer[current], game.wrongChoice1[current], game.wrongChoice2[current], game.wrongChoice3[current]];
 
 var randomArray = answerArray.sort(function() { return 0.5 - Math.random() });
@@ -33,10 +42,7 @@ var randomArray = answerArray.sort(function() { return 0.5 - Math.random() });
 console.log(answerArray);
 
 $(".question").show();
-$("#A").show();
-$("#B").show();
-$("#C").show();
-$("#D").show();
+
 $(".gamestatus").show();
 
 $(".question").html("Which " + game.industry[current] + " slogan is " + game.question[current] +"?");
@@ -51,21 +57,27 @@ $("#B").append(answerB);
 $("#C").append(answerC);
 $("#D").append(answerD);
 
-var windowTimeout = setTimeout(function(){
+
+
+windowTimeout = setTimeout(function(){
 
     $(".gamestatus").html("Time's Up!")
-    $(".question").html(" ");
-    $("#A").hide();
-    $("#B").hide();
-    $("#C").hide();
-    $("#D").hide();
+    $(".question").hide();
+    $(".question").empty();
+
+    $("#A").empty();
+    $("#B").empty();
+    $("#C").empty();
+    $("#D").empty();
     losses++;
+    
 
 }, 10000);
 
 $("button").on("click", function(){
     
     clearTimeout(windowTimeout);
+
 
     if (this.id === "A") {
 
@@ -87,57 +99,54 @@ $("button").on("click", function(){
 
      winLoss();
 
+
 });
-newQuestion();
+
+function resetGame(){
+
+    resetTimeout = setTimeout(function(){
+
+        newQuestion();
+
+    }, 1000);
 
 
+};
 
 function winLoss() { 
+    
 
     if (userChoice === game.correctAnswer[current]) {
 
+        clearTimeout(windowTimeout);
+
         $(".gamestatus").html("Correct! " + game.question[current] + " is the slogan for " + userChoice + ".");
         wins++;
+        resetGame();
 
-
-        var resetTimeout = setTimeout(function(){
-
-            $(".question").hide();
-            $("#A").hide();
-            $("#B").hide();
-            $("#C").hide();
-            $("#D").hide();
-            $(".gamestatus").hide();
-            newQuestion();
-
-        }, 5000);
 
     } else {
 
+        clearTimeout(windowTimeout);
+
         $(".gamestatus").html("Wrong!!");
         losses++;
+        resetGame();
+      
 
-        var resetTimeout = setTimeout(function(){
+    };
 
-            $(".question").hide();
-            $("#A").hide();
-            $("#B").hide();
-            $("#C").hide();
-            $("#D").hide();
-            $(".gamestatus").hide();
-            newQuestion();
+    
 
-        }, 5000);
-
-    }
-
-
+    $(".wins").html("Correct Answers: "+ wins);
+    $(".losses").html("Wrong Answers: "+ losses);
 
 }
 
 
 };
 
+newQuestion();
 
 
 
